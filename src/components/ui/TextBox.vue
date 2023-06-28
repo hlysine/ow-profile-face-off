e74972226fffd5e5391dc92f1b9d8449bf56e942
Henry Lin <henry.ys.lin@gmail.com>
2023-06-28 22:54:41 +0800

    Add player search function

---
+++ src/components/ui/TextBox.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = defineProps<{
+  modelValue: string;
+  placeholder?: string;
+  isValid?: boolean;
+}>();
+const emit = defineEmits<{
+  'update:modelValue': [text: string];
+}>();
+const text = computed({
+  get() {
+    return props.modelValue;
+  },
+  set(val: string) {
+    emit('update:modelValue', val);
+  },
+});
+</script>
+
+<template>
+  <input
+    v-model="text"
+    :placeholder="placeholder"
+    v-bind="$attrs"
+    class="bg-slate-800 p-3 rounded-md shadow-md text-slate-100 border focus:outline-none focus:border-slate-700 focus:shadow-lg placeholder:text-slate-500 transition-shadow"
+    :class="{ 'border-red-800 shadow-red-950 focus:shadow-red-950': !isValid, 'border-slate-800': isValid }"
+  />
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,61 @@
 <script setup lang="ts">
-import sample from '@/assets/sample1.json';
 import RankedPlayerCard from '@/components/overwatch/RankedPlayerCard.vue';
-import type { ProfileSummary } from '@/owProfile';
+import TextBox from '@/components/ui/TextBox.vue';
+import type { OWProfile } from '@/owProfile';
+import { computed, ref, watchEffect } from 'vue';
+import debounce from 'lodash/debounce';
+
+const battleTagRegex = /^[^#]+#\d+$/;
+
+const battleTag = ref('');
+const validationError = computed(() => {
+  const tag = battleTag.value;
+  if (tag.length === 0 || battleTagRegex.test(tag)) {
+    return null;
+  } else {
+    return 'Invalid Battle Tag';
+  }
+});
+
+const profile = ref<OWProfile | { error: string } | null>(null);
+const fetching = ref(false);
+
+async function fetchProfileRaw(player: string) {
+  fetching.value = true;
+  player = player.replace('#', '-');
+  const res = await fetch(
+    `https://api.allorigins.win/get?url=${encodeURIComponent(`https://overfast-api.tekrop.fr/players/${player}`)}`
+  );
+  const data: OWProfile | { error: string } = JSON.parse((await res.json()).contents);
+  profile.value = data;
+  fetching.value = false;
+}
+
+const fetchProfile = debounce(fetchProfileRaw, 500, {
+  leading: false,
+  trailing: true,
+});
+
+watchEffect(() => {
+  if (validationError.value || battleTag.value === '') {
+    profile.value = null;
+  } else {
+    fetchProfile(battleTag.value);
+  }
+});
 </script>
 
 <template>
-  <main>Placeholder main page</main>
-  <RankedPlayerCard class="m-4 shadow-lg" :profile="(sample.summary as ProfileSummary)" />
+  <div class="flex flex-col items-center">
+    <TextBox
+      v-model="battleTag"
+      class="m-4 w-[320px] mb-1"
+      :class="{ 'animate-pulse': fetching }"
+      placeholder="Type in your Battle Tag"
+      :is-valid="!validationError"
+    />
+    <span v-if="!!validationError" class="text-xs text-red-600">{{ validationError }}</span>
+    <span v-else-if="!!profile && 'error' in profile" class="leading-10">{{ profile.error }}</span>
+    <RankedPlayerCard v-else-if="!!profile" class="m-4 shadow-lg" :profile="profile.summary" />
+  </div>
 </template>
