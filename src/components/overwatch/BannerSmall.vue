184899d0ebcb0e0d0940b0838f25e6ba0d93f974
Henry Lin <henry.ys.lin@gmail.com>
2023-06-29 13:01:51 +0800

    Change banner

--- src/components/overwatch/BannerSmall.vue
+++ src/components/overwatch/BannerSmall.vue
@@ -6,8 +6,8 @@ import VueFeather from 'vue-feather';
   <div class="flex items-center gap-4">
     <VueFeather type="activity" class="w-24 h-24 text-blue-500"></VueFeather>
     <div class="banner-right">
-      <h1 class="text-blue-500 text-4xl italic font-extrabold">OW Face-off</h1>
-      <h3 class="text-gray-400 text-lg">Compare Overwatch profiles</h3>
+      <h1 class="text-blue-500 text-4xl italic font-extrabold">OW Profile+</h1>
+      <h3 class="text-gray-400 text-lg">Overwatch profiles better visualized</h3>
     </div>
   </div>
 </template>
