e9c47a3c5000c85498504e247e9cfb0e35126b41
Henry Lin <henry.ys.lin@gmail.com>
2023-07-09 19:19:34 +0800

    Remove debug component from statistics page

--- src/views/Profile/StatisticsView.vue
+++ src/views/Profile/StatisticsView.vue
@@ -1,16 +1,7 @@
-<script setup lang="ts">
-import HeroLeaderboard from '@/components/overwatch/HeroLeaderboard.vue';
-import type { OWProfile } from '@/owProfile';
-import { type Ref, inject, computed } from 'vue';
-
-const playerProfile = inject<Ref<OWProfile>>('player-profile')!;
-
-const data = computed(() => playerProfile.value.stats.pc!.quickplay!.heroes_comparisons.time_played.values);
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <div>
     <p class="m-16 text-xl text-center">This is the work-in-progress statistics view.</p>
-    <HeroLeaderboard :data="data" :title="playerProfile.stats.pc!.quickplay!.heroes_comparisons.time_played.label" />
   </div>
 </template>
